6499786673fcf4028955c79b8ac1b3f819507ce4
CORP\RamaShanker <ramashanker@fico.com>
2018-10-09 23:26:30 +0530

    added crud operation to mongo

--- mongodboperation/src/main/java/com/rama/mongo/operation/controller/OperationController.java
+++ mongodboperation/src/main/java/com/rama/mongo/operation/controller/OperationController.java
@@ -1,20 +1,30 @@
 package com.rama.mongo.operation.controller;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.rama.mongo.operation.exception.FileProcessException;
+import com.rama.mongo.operation.document.Employee;
+import com.rama.mongo.operation.exception.ProcessException;
 import com.rama.mongo.operation.response.FileUploadResponse;
 import com.rama.mongo.operation.response.RestResponse;
 import com.rama.mongo.operation.service.DataOperationService;
@@ -22,7 +32,8 @@ import com.rama.mongo.operation.service.DataOperationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-@Api(value="Mongo DB operation controller api",description="manage mongo crud operation")
+
+@Api(value = "Mongo DB operation controller api", description = "manage mongo crud operation")
 @RestController
 @RequestMapping("/mongo")
 public class OperationController {
@@ -31,24 +42,54 @@ public class OperationController {
 	public OperationController(final DataOperationService dataUploadService) {
 		this.dataUploadService = dataUploadService;
 	}
+
 	@ApiOperation(value = "upload json file to mongodb", response = RestResponse.class)
-	@RequestMapping(value = "entryfile", method = PUT, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "uploadfile", method = PUT, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<FileUploadResponse> bulkEntryFileUpload(
 			@ApiParam(name = "datafile", value = "json files to be uploaded", required = true) @RequestParam(value = "file", required = false) MultipartFile[] filesSubmitted,
 			@RequestParam(value = "name", required = false) String name) throws IOException {
 
 		if (filesSubmitted == null || filesSubmitted.length == 0) {
-			throw new FileProcessException("No files selected for update entries");
+			throw new ProcessException("No files selected for update entries");
 		}
 		if (name == null) {
-			throw new FileProcessException("No name selected for update entries");
+			throw new ProcessException("No name selected for update entries");
 		}
 		FileUploadResponse response = dataUploadService.uploadFileProcess(filesSubmitted, name);
 		int count = response.getFilecount();
-		if ( count == 0) {
+		if (count == 0) {
 			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
 		}
 		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@ApiOperation(value = "insert document to mongodb", response = Employee.class)
+	@RequestMapping(value = "create", method = POST, produces =  APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public Employee createData(@Valid @RequestBody Employee employee) {
+		return dataUploadService.createData(employee);	
+	}
+	
+	@ApiOperation(value = "get All document to mongodb", response = Employee.class)
+	@RequestMapping(value = "read", method = GET, produces =  APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public List<Employee> getAllData() {
+		return dataUploadService.readAllData();	
+	}
+	
+	@ApiOperation(value = "update document to mongodb", response = Employee.class)
+	@RequestMapping(value = "update/{id}", method = PUT, produces =  APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public Employee updateData(@PathVariable("id")ObjectId  id,@Valid @RequestBody Employee employee) {
+		employee.set_id(id);
+		return dataUploadService.updateData(employee);	
+	}
+	
+	@ApiOperation(value = "delete document to mongodb")
+	@RequestMapping(value = "delete/{id}", method = DELETE, produces =  APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public void updateData(@PathVariable("id")ObjectId  id) {
+		 dataUploadService.deleteData(id);	
 	}
 }
--- mongodboperation/src/main/java/com/rama/mongo/operation/service/DataOperationServiceImpl.java
+++ mongodboperation/src/main/java/com/rama/mongo/operation/service/DataOperationServiceImpl.java
@@ -9,51 +9,79 @@ import java.util.List;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.rama.mongo.operation.exception.FileProcessException;
+import com.rama.mongo.operation.document.Employee;
+import com.rama.mongo.operation.exception.ProcessException;
+import com.rama.mongo.operation.response.CrudResponse;
 import com.rama.mongo.operation.response.FileUploadResponse;
 
 @Service
-public class DataOperationServiceImpl implements DataOperationService{
+public class DataOperationServiceImpl implements DataOperationService {
 
 	private MongoTemplate mongoTemplate;
 
 	public DataOperationServiceImpl(MongoTemplate mongoTemplate) {
 		this.mongoTemplate = mongoTemplate;
 	}
-	
-	@Override
-	public FileUploadResponse uploadFileProcess(MultipartFile[] multipartFiles, String name)
-			throws FileProcessException {
-		FileUploadResponse uploadFileResponse = new FileUploadResponse();
-			List<String> exception = new ArrayList<>();
-			int counter = 0;
-			List<Document> documents = new ArrayList<>();
-			for (MultipartFile multipartFile : multipartFiles) {
-				String fileName = multipartFile.getOriginalFilename();
-				String fileNameWithoutExtension = FilenameUtils.removeExtension(fileName);
-				InputStream inputStream;
-				try {
-					inputStream = multipartFile.getInputStream();
-					String jsonTxt = IOUtils.toString(inputStream, "UTF-8");
-					Document doc = Document.parse(jsonTxt);
-					documents.add(doc);
-					mongoTemplate.insert(documents, fileNameWithoutExtension);
-				} catch (UnsupportedEncodingException ex) {
-					exception.add(ex.getMessage());
-					ex.printStackTrace();
-				} catch (IOException ex) {
-					exception.add(ex.getMessage());
-					ex.printStackTrace();
-				}
-				counter++;
 
+	@Override
+	public FileUploadResponse uploadFileProcess(MultipartFile[] multipartFiles, String name) throws ProcessException {
+		FileUploadResponse uploadFileResponse = new FileUploadResponse();
+		List<String> exception = new ArrayList<>();
+		int counter = 0;
+		List<Document> documents = new ArrayList<>();
+		for (MultipartFile multipartFile : multipartFiles) {
+			String fileName = multipartFile.getOriginalFilename();
+			String fileNameWithoutExtension = FilenameUtils.removeExtension(fileName);
+			InputStream inputStream;
+			try {
+				inputStream = multipartFile.getInputStream();
+				String jsonTxt = IOUtils.toString(inputStream, "UTF-8");
+				Document doc = Document.parse(jsonTxt);
+				documents.add(doc);
+				mongoTemplate.insert(documents, fileNameWithoutExtension);
+			} catch (UnsupportedEncodingException ex) {
+				exception.add(ex.getMessage());
+				ex.printStackTrace();
+			} catch (IOException ex) {
+				exception.add(ex.getMessage());
+				ex.printStackTrace();
 			}
-			uploadFileResponse.setFilecount(counter);
-			uploadFileResponse.setException(exception);
-			return uploadFileResponse;
+			counter++;
+
 		}
+		uploadFileResponse.setFilecount(counter);
+		uploadFileResponse.setException(exception);
+		return uploadFileResponse;
 	}
+
+	@Override
+	public Employee createData(Employee employee) {
+
+		employee.set_id(ObjectId.get());
+		mongoTemplate.insert(employee);
+		return employee;
+	}
+
+	@Override
+	public List<Employee> readAllData() {
+		return mongoTemplate.findAll(Employee.class);
+	}
+
+	@Override
+	public Employee updateData(Employee employee) {
+		mongoTemplate.save(employee);
+		return employee;
+	}
+
+	@Override
+	public void deleteData(ObjectId  id) {
+		mongoTemplate.remove(new Query(Criteria.where("_id").is(id)), Employee.class);
+	}
+}

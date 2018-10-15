3263f9b823332c51b3cd2c6c2121de06bb3d78db
ramashanker <ramashanker87@gmail.com>
2018-10-16 00:37:50 +0530

    updated transaction data

--- transaction-statistic/src/it/java/com/n26/HttpJsonIT.java
+++ transaction-statistic/src/it/java/com/n26/HttpJsonIT.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.n26.rama.app.Application;
+import com.rama.transaction.app.Application;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;

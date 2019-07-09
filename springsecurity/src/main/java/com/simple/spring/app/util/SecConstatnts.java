0fd3862f18bad61c8e3be9a19c5e7d438ecfabbd
a311057 <a311057@SEGOTW10347728.vcn.ds.volvo.net>
2019-07-09 16:30:28 +0200

    directory name change

--- springsecurity/src/main/java/com/simple/spring/app/util/SecConstatnts.java
+++
@@ -1,8 +0,0 @@
-package com.simple.spring.app.util;
-
-public class SecConstatnts {
-	
-    public static final String SWAGGER = "/swagger*";
-    public static final String ADMIN_URL = "/admin-name";
-    public static final String USER_URL = "/user-name";
-}

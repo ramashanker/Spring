931ebd4a1ab4908f31e6689179dfb1779c955a87
rama shanker <rama.shanker@wirelesscar.com>
2020-08-24 22:20:16 +0200

    changed library for artemis and property

--- artemis-demo/artemis-demo-producer/src/main/java/com/rama/artemis/config/ArtemisConfig.java
+++ artemis-demo/artemis-demo-producer/src/main/java/com/rama/artemis/config/ArtemisConfig.java
@@ -1,13 +1,18 @@
 package com.rama.artemis.config;
 
-import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.retry.annotation.EnableRetry;
+
+import java.util.Collections;
 
 @Configuration
+@EnableRetry
 public class ArtemisConfig {
     private static final Logger LOGGER = LoggerFactory.getLogger(ArtemisConfig.class);
     @Value("${artemis.enable.ssl}")
@@ -18,16 +23,18 @@ public class ArtemisConfig {
     private String user;
     @Value("${artemis.password}")
     private String password;
+    @Value("${artemis.connection.cache.size}")
+    private int sessionCacheSize;
 
-    @Bean
-    public ActiveMQConnectionFactory jmsFactory() {
-        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(brokerUrl);
-        factory.setUser(user);
-        factory.setPassword(password);
-        factory.setRetryInterval(1000);
-        factory.setRetryIntervalMultiplier(1.0);
-        factory.setReconnectAttempts(-1);
-        factory.setConfirmationWindowSize(10);
-        return factory;
+    @Bean(name = "jmsConnectionFactory")
+    public CachingConnectionFactory cachingConnectionFactory() {
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
+        activeMQConnectionFactory.setBrokerURL(brokerUrl);
+        activeMQConnectionFactory.setUserName(user);
+        activeMQConnectionFactory.setPassword(password);
+        activeMQConnectionFactory.setTrustedPackages(Collections.singletonList("com.volvocars.conncar.telematics"));
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(activeMQConnectionFactory);
+        cachingConnectionFactory.setSessionCacheSize(sessionCacheSize);
+        return cachingConnectionFactory;
     }
 }
--- artemis-demo/artemis-demo-producer/src/main/java/com/rama/artemis/producer/ArtemisProducer.java
+++ artemis-demo/artemis-demo-producer/src/main/java/com/rama/artemis/producer/ArtemisProducer.java
@@ -1,20 +1,32 @@
 package com.rama.artemis.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.UncategorizedJmsException;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ArtemisProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ArtemisProducer.class);
     @Autowired
     JmsTemplate jmsTemplate;
 
     @Value("${jms.queue.destination}")
     String destinationQueue;
-
-    public void send(String msg){
-        jmsTemplate.convertAndSend(destinationQueue, msg);
+    @Retryable(
+            value = {UncategorizedJmsException.class},
+            maxAttempts = 10,
+            backoff = @Backoff(random = true, delay = 1000, maxDelay = 8000,multiplier = 2)
+    )
+    public void send(String msg) {
+            LOGGER.info("Sending Data:");
+            jmsTemplate.convertAndSend(destinationQueue, msg);
+            LOGGER.info("Data Sent:");
     }
 }
 
